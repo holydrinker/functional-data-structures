811de151470c9c936e3a00e389095cb1433788e5
nicksaxo <37025755+n1CkS4x0@users.noreply.github.com>
2020-12-05 01:06:08 +0100

    Functions more concise

--- src/main/scala/sort/Sort.scala
+++ src/main/scala/sort/Sort.scala
@@ -3,8 +3,7 @@ package sort
 object Sort {
 
   private def isSorted(xs: List[Int]): Boolean =  xs match {
-    case Nil => true
-    case _ :: Nil => true
+    case Nil | _ :: Nil => true
     case h :: t if h <= t.head => isSorted(t)
     case _ => false
   }
@@ -13,8 +12,7 @@ object Sort {
     def oneRound(xs: List[Int]): List[Int] = xs match {
       case Nil => Nil
       case List(x) => List(x)
-      case h :: t if h > t.head => t.head :: oneRound(h :: t.tail)
-      case h :: t => h :: oneRound(t)
+      case h :: t => if (h > t.head) t.head :: oneRound(h :: t.tail) else h :: oneRound(t)
     }
 
     if(isSorted(xs)) xs
@@ -23,7 +21,6 @@ object Sort {
 
   def quicksort(xs: List[Int]): List[Int] = xs match {
     case Nil => Nil
-    case h :: Nil => List(h)
     case h :: t => quicksort(t.filter(_ < h)) ::: List(h) ::: quicksort(t.filter(_ >= h))
   }
 
